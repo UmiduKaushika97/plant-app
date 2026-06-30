480bc44ed20700b8fe8c542589bb9d74df6362d4
UmiduKaushika97 <umidu.kaushika@gmail.com>
2026-06-30 21:52:48 +0400

    navigation bar image add

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 
 import './App.css';
+import "./index.css";
+import image1 from "./assets/images/logo-inverted.png";
+
 
 
 function App() {
@@ -8,8 +11,16 @@ function App() {
     <>
    <main>
     <header>
-      <nav className='sticky top-0 bg-gradient-to-r from-rose-500 to pink-500 '> 
-        test
+      <nav className='sticky flex top-0 bg-gradient-to-r from-rose-500 to-pink-500 '> 
+
+    <div className='flex items-center p-2 gap-2'> 
+      <img src={image1} width={50}/>
+      <div className='text-2xl font-bold text-zinc-200'> 
+        tw:<span className='text-sky-900'>mf</span>
+
+      </div>
+    </div>
+
       </nav>
     </header>
    </main>
