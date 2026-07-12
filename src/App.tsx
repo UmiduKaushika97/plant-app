b6f4a7b19717899753cac6abd490e38895c82df4
UmiduKaushika97 <umidu.kaushika@gmail.com>
2026-07-12 12:21:53 +0400

    input field added

--- src/App.tsx
+++ src/App.tsx
@@ -138,9 +138,42 @@ function App() {
       className='flex flex-col items-center justify-center h-[calc(100vh-200px)] min-h-[400px] bg-cover bg-center bg-fixed'>
 <div className='bg-white/30 py-2 px-4 rounded-xl text-center backdrop-blur-md'>
       <div className='relative'>
-        <img src={image3} width={175}/>
+        <img src={image3} width={175} />
+        <div className='items-center flex justify-center mt-[-70px] mb-5'>
+          <div className='bg-pink-500 w-14 h-14 rounded-full flex items-center justify-center gap-1'>
+              <div className='h-2 w-1 bg-pink-300 rounded-full animate-wavey'></div>
+              <div className='h-3 w-1 bg-pink-200 rounded-full animate-wavey '></div>
+              <div className='h-4 w-1 bg-pink-100 rounded-full animate-wavey '></div>
+              <div className='h-3 w-1 bg-pink-200 rounded-full animate-wavey '></div>
+              <div className='h-2 w-1 bg-pink-300 rounded-full animate-wavey '></div>
+            
+          </div>
+        </div>
+      </div>
+
+      <div className='text-5xl font-bold'>
+        tw:<span className='text-sky-900'>mf</span>
       </div>
 </div>
+
+<div className='font-bold mt-3 text-sm'>
+  keep me update with news and promotions
+</div>
+
+<form className='mt-3 flex gap-2'>
+<input type='email' placeholder='Email' className='rounded-sm border border-white/40 bg-white/30 p-2 
+backdrop-blur-md font-bold text-sky-900 placeholder-zinc-500 caret-pink-500 outline-pink-500 
+'/>
+{/* focus:outline-500 */}
+<button 
+type='submit'
+className='coursor-pointer rounded-sm bg-pink-500 py-2 px-4 font-bold 
+transition-colors hover:bg-sky-900 hover:shadow-lg hover:shadow-black/20'>
+Subscribe
+</button>
+
+
+</form>
       </div>
 
     </header>
